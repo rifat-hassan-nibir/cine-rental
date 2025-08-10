57cdd38c7fa2d5bdb45114a3e5a0cd152f24924e
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2025-08-10 18:47:19 +0600

    feat: add movie list

---
+++ src/components/cine/MovieList.jsx
@@ -0,0 +1,18 @@
+import { getAllMovies } from "../../data/movies";
+import MovieCard from "./MovieCard";
+
+export default function MovieList() {
+  const movies = getAllMovies();
+
+  return (
+    <div>
+      <div className="content">
+        <div className="grid sm:grid-cols-2 xl:grid-cols-3 gap-7">
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movieDetails={movie} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}

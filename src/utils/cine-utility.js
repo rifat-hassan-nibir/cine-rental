a829cd989443d08f90af6040baeb73261a80fd03
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2025-08-11 20:49:42 +0600

    feat: function to get image url

--- src/utils/cine-utility.js
+++ src/utils/cine-utility.js
@@ -1,5 +1,5 @@
-function getImageUrl(name) {
-  return new URL(`../assets/movie-covers/${name}`, import.meta.url).href;
+function getImageUrl(imageTitle) {
+  return new URL(`../assets/movie-covers/${imageTitle}`, import.meta.url).href;
 }
 
 export { getImageUrl };
